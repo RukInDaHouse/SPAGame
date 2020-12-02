7af88cfddffcd4dd1784e4bf80da19fe599d9ee0
RukInDaHouse <r1997p@mail.ru>
2020-12-02 18:42:37 +0300

    Butify code

--- contact.js
+++ contact.js
@@ -1,3 +1,12 @@
 let contact = `
-  <h1>I am contact Page</h1>
+  <header></header>
+<div class="container">
+  <div class="ratio16-9">
+
+<div id="map" class="map small"><img src="./images/logwithoutclouds.svg" /></div>
+<div id="map2" class="map smallleft"><img src="./images/logwithoutclouds.svg" /></div>
+<div id="map3" class="map smallright"><img src="./images/logwithoutclouds.svg" /></div>
+<div id="map4" class="map smallright"><img src="./images/logwithoutclouds.svg" /></div>
+</div>
+</div>
 `
