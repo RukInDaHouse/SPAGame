e918e216660d173b67033af2d96bd69222712cc2
RukInDaHouse <r1997p@mail.ru>
2020-12-02 18:15:47 +0300

    Create home js
    Create home js with map

---
+++ home.js
@@ -0,0 +1,11 @@
+let home = `
+<div class="container">
+  <div class="ratio16-9">
+<a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 1550); return false;"><div id="map" class="map"><img src="./images/logwithoutclouds.svg" /></div></a>
+<div id="map2" class="map"><img src="./images/logwithoutclouds.svg" /></div>
+<div id="map3" class="map"><img src="./images/logwithoutclouds.svg" /></div>
+<div id="map4" class="map"><img src="./images/logwithoutclouds.svg" /></div>
+
+  </div>
+  </div>
+`
