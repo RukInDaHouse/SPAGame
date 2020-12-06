e5619ed22e3632b5f006d4afa43be3d2d06f2c5e
RukInDaHouse <r1997p@mail.ru>
2020-12-06 04:18:34 +0300

    Refactoring

--- templates/contact.js
+++ templates/contact.js
@@ -1,12 +1,26 @@
 let contact = `
-  <header></header>
-<div class="container">
-  <div class="ratio16-9">
 
-<div id="map" class="map small"><img src="./images/logwithoutclouds.svg" /></div>
-<div id="map2" class="map smallleft"><img src="./images/logwithoutclouds.svg" /></div>
-<div id="map3" class="map smallright"><img src="./images/logwithoutclouds.svg" /></div>
-<div id="map4" class="map smallright"><img src="./images/logwithoutclouds.svg" /></div>
-</div>
+  <header></header>
+
+<div class="container">
+    <div class="ratio16-9">
+
+        <div id="map" class="map small">
+            <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div id="map2" class="map smallleft">
+            <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div id="map3" class="map smallright">
+            <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div id="map4" class="map smallright">
+            <img src="./images/logwithoutclouds.svg" />
+        </div>
+    
+    </div>
 </div>
 `
