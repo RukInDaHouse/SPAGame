8334563b80208ac4d642c2677f31cdfd5cecc511
RukInDaHouse <r1997p@mail.ru>
2020-12-06 04:18:22 +0300

    Refactoring

--- templates/about.js
+++ templates/about.js
@@ -1,15 +1,25 @@
 let about = `
+
 <div class="container">
   <div class="ratio16-9">
-<div class="backgroundclose"><img src="./images/logwithoutclouds.svg" /></div>
-<div class="flashfast backgroundfar"><img src="./images/logman.svg" /></div>
-<a href="#" onclick="setTimeout(function(){ onNavigate('/contact'); }, 1550); return false;"><div class="arrowcontainer flashfast">
-    <div class="arrowbtn">
-    <img src="./images/button.svg">
+
+    <div class="backgroundclose">
+        <img src="./images/logwithoutclouds.svg" />
     </div>
-  </div></a>
+
+    <div class="flashfast backgroundfar">
+        <img src="./images/logman.svg" />
+    </div>
+
+    <a href="#" onclick="setTimeout(function(){ onNavigate('/contact'); }, 1550); return false;">
+        <div class="arrowcontainer flashfast">
+            <div class="arrowbtn">
+                <img src="./images/button.svg">
+            </div>
+        </div>
+    </a>
 
   </div>
-  </div>
+</div>
 
 `
