4e91fa556ce17047a30158bfe6d9021ec93922be
RukInDaHouse <r1997p@mail.ru>
2020-12-07 15:56:09 +0300

    new grid

--- templates/home.js
+++ templates/home.js
@@ -1,26 +1,87 @@
 let home = `
 
 <div class="container">
-  <div class="ratio16-9">
-
-    <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 1550); return false;">
-        <div id="map" class="map">
-            <img src="./images/logwithoutclouds.svg" />
+    <div class="ratio16-9 small">
+        <div class="col-lg-3 map line-1-elem-1">
         </div>
-    </a>
+        <div class="col-lg-2 map line-1-elem-2 ">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+        <div class="col-lg-7 map line-1-elem-3">
+          
+        </div>
 
-    <div id="map2" class="map">
-        <img src="./images/logwithoutclouds.svg" />
-    </div>
+        <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 1550); return false;">
+            <div class="col-lg-2 map line-2-elem-1">
+                <img src="./images/logwithoutclouds.svg" />
+            </div>
+        </a>
 
-    <div id="map3" class="map">
-        <img src="./images/logwithoutclouds.svg" />
-    </div>
+        <div class="col-lg-4 map line-2-elem-2">
+        </div>
 
-    <div id="map4" class="map">
-        <img src="./images/logwithoutclouds.svg" />
-    </div>
+        <div class="col-lg-2 map line-2-elem-3">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
 
+        <div class="col-lg-4 map line-2-elem-4">
+          
+        </div>
+
+
+        <div class="col-lg-2 map line-3-elem-1">
+        </div>
+
+        <div class="col-lg-2 map line-3-elem-2">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-4 map line-3-elem-3">
+        </div>
+
+        <div class="col-lg-2 map line-3-elem-4">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-2 map line-3-elem-5">
+        </div>
+
+        <div class="col-lg-3 map line-4-elem-1">
+        </div>
+
+
+        <div class="col-lg-2 map line-4-elem-2">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-7 map line-4-elem-3">
+        </div>
+
+
+        <div class="col-lg-2 map line-5-elem-1">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-2 map line-5-elem-2">
+        </div>
+
+        <div class="col-lg-2 map line-5-elem-3">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-2 map line-5-elem-4">
+        </div>
+
+        <div class="col-lg-2 map line-5-elem-5">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-2 map line-5-elem-6">
+          <img src="./images/logwithoutclouds.svg" />
+        </div>
+
+        <div class="col-lg-12 line-6-elem-1 map2"></div>
   </div>
+
 </div>
 `
