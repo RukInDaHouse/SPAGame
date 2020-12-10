352353d6ed5f1939b17634709e1511b224cf2004
RukInDaHouse <r1997p@mail.ru>
2020-12-10 18:26:25 +0300

    New about.js
    Changes in layout. add new images and new animations

--- templates/about.js
+++ templates/about.js
@@ -1,25 +1,21 @@
 let about = `
 
 <div class="container">
-  <div class="ratio16-9">
+    <div class="ratio16-9">
 
-    <div class="backgroundclose">
-        <img src="./images/logwithoutclouds.svg" />
-    </div>
+        <div class="backgroundclose">
+            <img src="./images/part-01.svg" />
+        </div>
 
-    <div class="flashfast backgroundfar">
-        <img src="./images/logman.svg" />
-    </div>
-
-    <a href="#" onclick="setTimeout(function(){ onNavigate('/contact'); }, 1550); return false;">
-        <div class="arrowcontainer flashfast">
-            <div class="arrowbtn">
-                <img src="./images/button.svg">
+        <a href="#" onclick="setTimeout(function(){ onNavigate('/contact'); }, 1550); return false;">
+            <div class="arrowcontainer flashfast">
+                <div class="arrowbtn">
+                    <img src="./images/button.svg">
             </div>
         </div>
-    </a>
+        </a>
 
-  </div>
+    </div>
 </div>
 
 `
