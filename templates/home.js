b1ec76cc1f3ab565ad094eaff6394b7a41593793
RukInDaHouse <r1997p@mail.ru>
2020-12-10 18:26:55 +0300

    New home js
    Changes in layout. add new images and new animations

--- templates/home.js
+++ templates/home.js
@@ -1,87 +1,31 @@
 let home = `
 
 <div class="container">
-    <div class="ratio16-9 small">
-        <div class="col-lg-3 map line-1-elem-1">
+    <div class="ratio16-9 map">
+
+        <div class="background-01-01 ">
+            <img src="./images/01-01-withoutplane.svg" /><br>
+            <img src="./images/02-01.svg" /><br>
+            <img src="./images/03-01.svg" />
         </div>
-        <div class="col-lg-2 map line-1-elem-2 ">
-          <img src="./images/logwithoutclouds.svg" />
+        
+        <div class="background-01-02 ">
+            <img src="./images/01-02.svg" /><br>
+            <img src="./images/02-02.svg" /><br>
+            <img src="./images/03-02.svg" />
         </div>
-        <div class="col-lg-7 map line-1-elem-3">
-          
+        
+        <div class="background-01-03 ">
+            <img src="./images/01-03.svg" /><br>
+            <img src="./images/02-03.svg" /><br>
+            <img src="./images/03-03.svg" />
         </div>
-
-        <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 1550); return false;">
-            <div class="col-lg-2 map line-2-elem-1">
-                <img src="./images/logwithoutclouds.svg" />
-            </div>
-        </a>
-
-        <div class="col-lg-4 map line-2-elem-2">
-        </div>
-
-        <div class="col-lg-2 map line-2-elem-3">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-4 map line-2-elem-4">
-          
-        </div>
-
-
-        <div class="col-lg-2 map line-3-elem-1">
-        </div>
-
-        <div class="col-lg-2 map line-3-elem-2">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-4 map line-3-elem-3">
-        </div>
-
-        <div class="col-lg-2 map line-3-elem-4">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-2 map line-3-elem-5">
-        </div>
-
-        <div class="col-lg-3 map line-4-elem-1">
-        </div>
-
-
-        <div class="col-lg-2 map line-4-elem-2">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-7 map line-4-elem-3">
-        </div>
-
-
-        <div class="col-lg-2 map line-5-elem-1">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-2 map line-5-elem-2">
-        </div>
-
-        <div class="col-lg-2 map line-5-elem-3">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-2 map line-5-elem-4">
-        </div>
-
-        <div class="col-lg-2 map line-5-elem-5">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-2 map line-5-elem-6">
-          <img src="./images/logwithoutclouds.svg" />
-        </div>
-
-        <div class="col-lg-12 line-6-elem-1 map2"></div>
   </div>
-
+    
+    <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 3550); return false;">
+        <div class="plane_block map">
+            <img src="./images/part-01.svg" />
+        </div>
+    </a>
 </div>
 `
