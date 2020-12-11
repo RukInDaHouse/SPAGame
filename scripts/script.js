c2371789e864e2e4561d6a4702f0dea181953b94
RukInDaHouse <r1997p@mail.ru>
2020-12-11 17:40:28 +0300

    Add new anomations script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 var map = document.getElementsByClassName('map');
+var helloscreen = document.getElementsByClassName('helloscreen');
 
-  map[1].onclick = function() {
-  map[0].classList.add('zoom-in'),
-  map[1].classList.add('zoom-in-plane');
+    map[2].onclick = function() {
+    helloscreen[0].classList.add('invisible'),
+    map[0].classList.add('zoom-in'),
+    map[3].classList.add('zoom-in-plane');
+}
+
+    map[0].onclick = function() {
+    helloscreen[0].classList.add('flashmodal');
 }
