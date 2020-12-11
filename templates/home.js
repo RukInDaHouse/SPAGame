e6c1cca9f24fe4f56213717d3b39874f35ca129e
RukInDaHouse <r1997p@mail.ru>
2020-12-11 17:39:43 +0300

    Add helloscreen in home js

--- templates/home.js
+++ templates/home.js
@@ -2,10 +2,23 @@ let home = `
 
 <div class="container">
     <div class="ratio16-9 map">
-
-        <div class="background-01-01 ">
+        <div class="helloscreen">
+            <div class="previewtext flashmodal map">
+                <h1 class="underline">Уважаемый герой!</h1>
+                <p>Сейчас мы проводим тестирование и твое мнение очень важно для нас. На этапе регистрации мы попросим указать твоё имя и телефон. На твой номер придёт код для подтверждения, введя его ты дашь согласие на звонок и сможешь продолжить дальше. </p>
+                <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 3550); return false;">
+                <div class="previewbutton map">OK
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="mapbackgroundimage">
+            <img src="./images/backgroundmap.svg" />
+        </div>
+        
+        <div class="background-01-01">
             <img src="./images/01-01-withoutplane.svg" /><br>
-            <img src="./images/02-01.svg" /><br>
+            <img src="./images/02-01-withoutplane.svg" /><br>
             <img src="./images/03-01.svg" />
         </div>
         
@@ -20,12 +33,10 @@ let home = `
             <img src="./images/02-03.svg" /><br>
             <img src="./images/03-03.svg" />
         </div>
-  </div>
-    
-    <a href="#" onclick="setTimeout(function(){ onNavigate('/about'); }, 3550); return false;">
-        <div class="plane_block map">
-            <img src="./images/part-01.svg" />
-        </div>
-    </a>
+    </div>
+
+    <div class="plane_block map">
+        <img src="./images/part-01.svg" />
+    </div>
 </div>
 `
