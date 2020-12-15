d65e4ca707819a281dddd7ff114ca2ba7a55905f
RukInDaHouse <r1997p@mail.ru>
2020-12-15 16:40:14 +0300

    Add pop-up scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 var map = document.getElementsByClassName('map');
 var helloscreen = document.getElementsByClassName('helloscreen');
+var header = document.getElementsByClassName('headercontainer');
 
     map[2].onclick = function() {
     helloscreen[0].classList.add('invisible'),
     map[0].classList.add('zoom-in'),
     map[3].classList.add('invisible');
     map[4].classList.add('zoom-in-plane');
+    header[0].classList.add('invisible');
 }
 
     map[0].onclick = function() {
