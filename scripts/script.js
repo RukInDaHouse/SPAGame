d4b6398f3a05b07de316cb253e7eafb59d17d65e
RukInDaHouse <r1997p@mail.ru>
2020-12-15 17:04:26 +0300

    add hide header scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 var map = document.getElementsByClassName('map');
 var helloscreen = document.getElementsByClassName('helloscreen');
 var header = document.getElementsByClassName('headercontainer');
-
+var container = document.getElementsByClassName('container');
     map[2].onclick = function() {
     helloscreen[0].classList.add('invisible'),
     map[0].classList.add('zoom-in'),
+    container[0].classList.add('zerotop'),
     map[3].classList.add('invisible');
     map[4].classList.add('zoom-in-plane');
     header[0].classList.add('invisible');
