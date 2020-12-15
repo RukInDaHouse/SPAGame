807ff107c90812e9052c2a6a9d6cc330a007385b
RukInDaHouse <r1997p@mail.ru>
2020-12-15 17:33:56 +0300

    fix script bug

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,6 @@ var container = document.getElementsByClassName('container');
     map[2].onclick = function() {
     helloscreen[0].classList.add('invisible'),
     map[0].classList.add('zoom-in'),
-    container[0].classList.add('zerotop'),
     map[3].classList.add('invisible');
     map[4].classList.add('zoom-in-plane');
     header[0].classList.add('invisible');
