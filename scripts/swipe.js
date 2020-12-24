408f5b57961ea94ca018581799946211e996d0c6
RukInDaHouse <r1997p@mail.ru>
2020-12-24 17:34:27 +0300

    New card script

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,17 +1,16 @@
 'use strict';
 
-var quizcardContainer = document.querySelector('.quiz__container');
-var allCards = document.querySelectorAll('.quizcard--card');
+var quizcardContainer = document.querySelector('.quizcard');
+var allCards = document.querySelectorAll('.flip-card');
 var nope = document.getElementById('nope');
 var love = document.getElementById('love');
-var cardtext = document.getElementsByClassName('cardtext');
+var next = document.getElementById('next');
 
 function initCards(card, index) {
-  var newCards = document.querySelectorAll('.quizcard--card:not(.removed)');
+  var newCards = document.querySelectorAll('.flip-card:not(.removed)');
 
   newCards.forEach(function (card, index) {
     card.style.zIndex = allCards.length - index;
-    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
     card.style.opacity = (10 - index) / 10;
   });
   
@@ -22,8 +21,8 @@ initCards();
 
 function createButtonListener(love) {
   return function (event) {
-    var cards = document.querySelectorAll('.quizcard--card:not(.removed)');
-    var moveOutWidth = document.body.clientWidth * 1.5;
+    var cards = document.querySelectorAll('.flip-card:not(.removed)');
+    var moveOutWidth = document.body.clientWidth * 0.3;
 
     if (!cards.length) return false;
 
@@ -32,11 +31,13 @@ function createButtonListener(love) {
     card.classList.add('removed');
 
     if (love) {
-      card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(0deg)';
-    } else {
-      card.style.transform = 'translateX(0%) rotateY(-180deg)';
-      card.style.zIndex = '99999';
-    }
+      card.style.transform = 'translate(' + moveOutWidth + 'px, -900px) rotate(0deg)';
+    } else if (nope){
+      card.classList.toggle('do-flip');
+      document.getElementById('next').onclick = function() {
+        card.style.transform = 'translate(' + moveOutWidth + 'px, 900px)';
+  };
+    } 
 
     initCards();
 
@@ -44,12 +45,14 @@ function createButtonListener(love) {
   };
 }
 
+
+
 var nopeListener = createButtonListener(false);
 var loveListener = createButtonListener(true);
 
 nope.addEventListener('click', nopeListener);
 love.addEventListener('click', loveListener);
 
-nope.onclick = function() {
-  cardtext[0].classList.remove('invisible');
-  }
+
+
+
